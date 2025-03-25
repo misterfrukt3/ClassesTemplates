7646d951e8e3af65e28ef9237a9e8d158a0f96fe
misterfrukt <misterfrukt3@gmail.com>
2025-03-25 17:23:28 +0300

    add part2

---
+++ Planet/using_lib/planet.h
@@ -0,0 +1,53 @@
+#ifndef PLANET_H
+#define PLANET_H
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+enum MenuOption {
+    READ_FROM_FILE = 1,
+    WRITE_TO_FILE,
+    EDIT_PLANET,
+    PRINT_BOOKS,
+    SORT_BY_DIAMETER,
+    SORT_BY_NAME,
+    ADD_BOOK,
+    DELETE_BOOK,
+    EXIT_PROGRAM
+};
+
+class Planet {
+private:
+    char* planet_;
+    int diameter_, life_, satellite_;
+
+public:
+    Planet();
+    Planet(const char* planet, int diameter, int life, int satellite);
+    Planet(const Planet& p);
+    Planet& operator=(const Planet& other);
+    ~Planet();
+
+    friend std::istream& operator>>(std::istream& in, Planet& planet);
+    friend std::ostream& operator<<(std::ostream& os, const Planet& planet);
+
+    const char* getPlanet() const;
+    int getDiameter() const;
+    int getLife() const;
+    int getSatellite() const;
+
+    void clear();
+
+    static void add_planet(Planet*& planets, int& n_planet, int& capacity);
+    static void delete_planet(Planet* planets, int& n_planet, int planet_index);
+    static int read_db(const char* file_name, Planet*& planets, int& n_planet, int& capacity);
+    static int menu();
+    static void print_db(const Planet* planets, int n_planet);
+    static int write_db(const char* file_name, const Planet* planets, int n_planet);
+    static int find(const Planet* planets, int n_planet);
+    static void sort_by_diameter(Planet* planets, int n_planet);
+    static void sort_by_name(Planet* planets, int n_planet);
+};
+
+#endif // PLANET_H
--- build/CMakeFiles/planet_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libplanet_lib.a"
-)
