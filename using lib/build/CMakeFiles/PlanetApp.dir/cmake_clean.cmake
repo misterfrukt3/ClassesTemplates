fbb2641f5a7afb38e1cd1d36f496ee6a62aab16a
misterfrukt <misterfrukt3@gmail.com>
2025-03-25 16:20:07 +0300

    t

---
+++ using lib/build/CMakeFiles/PlanetApp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PlanetApp.dir/link.d"
+  "CMakeFiles/PlanetApp.dir/main.cpp.o"
+  "CMakeFiles/PlanetApp.dir/main.cpp.o.d"
+  "PlanetApp"
+  "PlanetApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PlanetApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
